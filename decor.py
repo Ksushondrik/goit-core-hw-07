f635ab9a7cfb52b602d7e95d05f931d053e48057
Ksushondrik <ksushondrik369@gmail.com>
2024-03-06 13:46:07 +0200

    Version_3.2

--- decor.py
+++ decor.py
@@ -3,12 +3,21 @@ def input_error(func):
     def inner(*args, **kwargs):
         try:
             return func(*args, **kwargs)
-        except ValueError:
-            return "Enter the argument for the command. Give me name and phone or birthday, please."
-        except KeyError:
-            return "No such name found"
-        except IndexError:
-            return "Enter the argument for the command. Give me name, please."
+        except ValueError as e:
+            if e is None:
+                return "Enter the argument for the command. Give me name and phone or birthday, please."
+            else:
+                return f"Error: {e}"
+        except KeyError as e:
+            if e is None:
+                return "No such name found"
+            else:
+                return f"Error: {e}"
+        except IndexError as e :
+            if e is None:
+                return "Enter the argument for the command. Give me name, please."
+            else:
+                return f"Error: {e}"
         except Exception as e:
             return f"Error: {e}"
 
